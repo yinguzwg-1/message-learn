a05f190feee8b7623d3dec0698b64c4e34a0d2e1
yinguzwg <976108364@qq.com>
2024-06-17 11:23:37 +0800

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const repo = "message-pro";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
 const nextConfig = {
-  basePath,
-  assetPrefix,
-  output: "export",
   experimental: {
     appDir: true,
     swcPlugins: [["next-superjson-plugin", {}]],
